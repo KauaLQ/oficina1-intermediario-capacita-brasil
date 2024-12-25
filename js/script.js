51f2fb3dbe2b66fd1cf1244d94f07105d119d12b
KauaLQ <limakaua610@gmail.com>
2024-12-24 21:33:06 -0300

    Lógica imc terminada

--- js/script.js
+++ js/script.js
@@ -26,6 +26,24 @@ function formatInputWeight(input){
     input.value = value;
   }
 
+function resetBorders() {
+    const ids = [
+        "obesidade3",
+        "obesidade2",
+        "obesidade1",
+        "acima-peso",
+        "peso-normal",
+        "abaixo-peso"
+    ];
+
+    ids.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.classList.remove("stylized");
+        }
+    });
+}
+
 function calcularIMC(){
     let peso = parseFloat(document.getElementById("peso").value);
     let altura = parseFloat(document.getElementById("altura").value);
@@ -40,4 +58,35 @@ function calcularIMC(){
         imcElement.innerHTML = imc;
         imcElement.classList.add("show");
     }, 100);
+
+    if (imc > 40) {
+      resetBorders();
+      let component = document.getElementById("obesidade3");
+      component.classList.toggle("stylized");
+    }
+    else if (imc < 40 && imc >= 35){
+      resetBorders();
+      let component = document.getElementById("obesidade2");
+      component.classList.add("stylized");
+    }
+    else if (imc < 35 && imc >= 30){
+      resetBorders();
+      let component = document.getElementById("obesidade1");
+      component.classList.add("stylized");
+    }
+    else if (imc < 30 && imc >= 25){
+      resetBorders();
+      let component = document.getElementById("acima-peso");
+      component.classList.add("stylized");
+    }
+    else if (imc < 25 && imc >= 18.6){
+      resetBorders();
+      let component = document.getElementById("peso-normal");
+      component.classList.add("stylized");
+    }
+    else if (imc < 18.6){
+      resetBorders();
+      let component = document.getElementById("abaixo-peso");
+      component.classList.add("stylized");
+    }
 }
